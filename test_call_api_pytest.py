aa3aa84a822c3bbcdd9422e0af668de84910df79
André Fettouhi <A.Fettouhi@gmail.com>
2019-06-17 16:08:23 +0200

    Added two more tests!

--- test_call_api_pytest.py
+++ test_call_api_pytest.py
@@ -9,3 +9,20 @@ def test_call_api_500():
     responses.add(responses.GET, url, status=500)
     result = call_api(url)
     assert result is False
+
+
+@responses.activate
+def test_call_api_200():
+    url = 'http://globomantics.com/api'
+    responses.add(responses.GET, url, status=200)
+    result = call_api(url)
+    assert result is True
+
+
+@responses.activate
+def test_call_api_404():
+    url = 'http://globomantics.com/api'
+    responses.add(responses.GET, url, status=404)
+    result = call_api(url)
+    assert result is False
+
